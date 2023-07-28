e5aa1f08f93539a77722bf9b7b743d24074572cc
William Sumida <william.h.sumida@gmail.com>
2023-07-28 08:43:25 -0300

    docker setup

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:18-alpine AS build
+WORKDIR /api/
+COPY package.json package-lock.json ./
+RUN npm install -g npm@8.19.2
+RUN npm install
+COPY . .
+COPY .env.example .env
+
+CMD [ "npm", "run", "generate" ]
+CMD [ "npm", "run", "dev" ]
+
--- src/database/schema/farm.ts
+++ src/database/schema/farm.ts
@@ -8,9 +8,13 @@ export const farm = pgTable("farm", {
   area: numeric("area"),
   cropArea: numeric("crop_area"),
   vegetationArea: numeric("vegetation_area"),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow()
 });
 
 export const farmCrop = pgTable("farm_crop", {
   farmerId: text("crop_id").references(() => crop.id),
   farmId: text("farm_id").references(() => farm.id),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow()
 });
